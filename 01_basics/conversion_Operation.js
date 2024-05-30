a8b42f55d0ed1d838f0091b66806f5c581b11dfb
ayushsinghDTU <148863362+ayushsinghDTU@users.noreply.github.com>
2024-05-30 06:49:55 +0000

    conersionOfDatatypes

---
+++ 01_basics/conversion_Operation.js
@@ -0,0 +1,22 @@
+let score = "33abs";
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score); // conersion in number 'N'-> capital 
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);// NaN ->value 
+
+/*
+" 33" => 33
+"33abs" => NaN
+true=>1 ; false =>0;
+*/
+let isLoggedIn = 1;
+let booleanIsLogIn = Boolean(isLoggedIn);
+//console.log(booleanIsLogIn);
+// 1->true ; 0=> false 
+// "" =>false
+// "ayush" => true;
+// String for string conversion
+
