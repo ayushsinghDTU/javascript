392cd0f3ab3e34e329a2f49b5cdce639f1968fd9
ayushsinghDTU <148863362+ayushsinghDTU@users.noreply.github.com>
2024-05-30 07:29:09 +0000

    summary_datatypes

---
+++ 01_basics/comparison_datatyp.js
@@ -0,0 +1,10 @@
+//console.log("2" > 1) // datatypes should be same 
+
+console.log(null > 0);
+console.log(null == 0);
+console.log (null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log (undefined >= 0);
+
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,11 @@
+// # Primitive
+// 7 types : string ,number ,Boolearn , null, undefined ,symbol, Bigint
+
+
+// # Reference (Non primitive)
+// Array ,objects, Functions
+
+const Id = Symbol("1234");
+const anotherId = Symbol("1234")
+console.log(Id == anotherId); 
+
